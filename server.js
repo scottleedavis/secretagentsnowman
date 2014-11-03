e3d51104f65c2436c5e0707f490912eb3eb70741
Scott Davis <scott.davis@webtrends.com>
2014-11-03 10:04:14 -0800

    updated express version and package.json.

--- server.js
+++ server.js
@@ -4,13 +4,10 @@ var express = require('express');
  
 
 var app = express();
-app.use(express.bodyParser());
-
-
+app.set('port', (process.env.PORT || 5000))
 app.use("/", express.static(__dirname + '/public'));
 
-require('express-debug')(app, {/* settings */});
 
-
-app.listen(3000);
-console.log('Listening on port 3000...');
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
