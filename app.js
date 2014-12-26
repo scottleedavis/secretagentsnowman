bb0599efefce27f470969b1fe60161375ea0d497
skawtus <skawtus@gmail.com>
2014-12-26 13:01:00 -0800

    remove toss for now.

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var toss = require(path.join(__dirname, 'lib/snowball'));
+//toss costs money!  enable later on...
+//var toss = require(path.join(__dirname, 'lib/snowball'));
 var app = express();
 
 app.enable("trust proxy");
@@ -19,7 +20,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(toss());
+//app.use(toss());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
